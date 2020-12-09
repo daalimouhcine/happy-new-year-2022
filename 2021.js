f536799145ab75d42b39058deaa06f93ea201263
daalimouhcine <daalim277@gmail.com>
2020-12-09 19:56:19 +0100

    Add files via upload

--- 2021.js
+++ 2021.js
@@ -9,6 +9,7 @@ const input = document.querySelector("#input");
 const name = document.getElementsByClassName("name");
 const submit = document.querySelector("#submit-whatsapp");
 const form = document.querySelector("#form");
+const sound = document.querySelector("#sound");
 
 
 // ----- EVENTS her
@@ -23,15 +24,21 @@ function openDoor() {
     body.classList.add("open");
     body.style.overflow ="auto";
     body.style.background = `url(images/background-2.gif)`;
+    sound.innerHTML = `<audio src="audio/aid.mp3" autoplay></audio>`;
 }
 function buttonName(e) {
     e.preventDefault();
-    if(input.value !== "") {
-        for(let i = 0 ; i < name.length ; i++) {
-            name[i].innerText = input.value;
-        };        input.style.display = "none";
-        button.style.display = "none";
-        form.innerHTML = `<a href="whatsapp://send?text=🙌 Une surprise spéciale pour toi de la part de 👉 *${input.value}* 👈 %0AClique sur la ligne bleue pour voir %0A‼️👇👇👇👇‼️%0A  http://127.0.0.1:5500/2021%20ann%C3%A9e/2021.html?name=${input.value}" id="submit-whatsapp"><img src="images/whatsapp.png" alt=""> <b>👈 Partagez</b> </a>`;
-    }
-
+    for(let i = 0 ; i < name.length ; i++) {
+        name[i].innerText = input.value;
+    };        input.style.display = "none";
+    button.style.display = "none";
+    form.innerHTML = `<a href="whatsapp://send?text=🙌 Une surprise spéciale pour toi de la part de 👉 *${input.value}* 👈 %0AClique sur la ligne bleue pour voir %0A‼️👇👇👇👇‼️%0A  http://127.0.0.1:5500/2021%20ann%C3%A9e/2021.html?name=${input.value}" id="submit-whatsapp"><img src="images/whatsapp.png" alt=""> <b>👈 Partagez</b> `;
 }
+
+
+
+/*
+if(name[0].innerText.length === 0) {
+    alert("dkjdf");
+}
+*/
